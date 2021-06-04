eebf132e1fe3a618b9525d0bd992eda20cdc148a
상민 박 <smpark95@mindslab.ai>
2021-06-04 11:36:10 -0400

    Add required entities. Close #4.

---
+++ src/main/java/com/rsmp/redditclone/model/entity/Post.java
@@ -0,0 +1,50 @@
+package com.rsmp.redditclone.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Post name cannot be null or empty")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    private Integer voteCount;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name="user_id", referencedColumnName = "id")
+    private User user;
+
+    private Instant createDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "subreddit_id", referencedColumnName = "id")
+    private Subreddit subreddit;
+
+
+
+}
