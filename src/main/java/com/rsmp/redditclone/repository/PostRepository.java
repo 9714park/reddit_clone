8cb3b63ff919dc6a72564fdb6dd5990b99821324
rsmpark <smpark95@mindslab.ai>
2021-06-13 13:55:56 -0400

    Add repositories for entities. Closes #5.

---
+++ src/main/java/com/rsmp/redditclone/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.rsmp.redditclone.repository;
+
+import com.rsmp.redditclone.model.entity.Post;
+import com.rsmp.redditclone.model.entity.Subreddit;
+import com.rsmp.redditclone.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+    List<Post> findBySubreddit(Subreddit subreddit);
+
+    List<Post> findByUser(User user);
+}
