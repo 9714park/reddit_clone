1f5023f6d385582cd62b0ceec1f718b7e88cd183
rsmpark <smpark95@mindslab.ai>
2021-08-18 15:53:51 -0400

    Complete signup post request. Closes #2

--- src/main/java/com/rsmp/redditclone/controller/AuthController.java
+++ src/main/java/com/rsmp/redditclone/controller/AuthController.java
@@ -1,17 +1,26 @@
 package com.rsmp.redditclone.controller;
 
 import com.rsmp.redditclone.model.dto.RegisterRequest;
+import com.rsmp.redditclone.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
-    @PostMapping("signup")
-        public void signup(@RequestBody RegisterRequest registerRequst){
 
-        }
+    private final AuthService authService;
+
+    @PostMapping("signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful", HttpStatus.OK);
+    }
 
 }
