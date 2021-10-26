279efe6996c473b60ae00d653c7e4dae3eec287c
rsmpark <smpark95@mindslab.ai>
2021-10-26 11:03:14 -0400

    Connect sign-up UI with auth service.

--- reddit-clone-front/src/app/auth/sign-up/sign-up.component.ts
+++ reddit-clone-front/src/app/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { SignupRequestPayload } from './sign-up-request.payload';
 
 @Component({
   selector: 'app-sign-up',
@@ -8,14 +10,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignUpComponent implements OnInit {
   signupForm!: FormGroup;
+  signupRequestPayload: SignupRequestPayload;
 
-  constructor() {}
+  constructor(private authService: AuthService) {
+    this.signupRequestPayload = {
+      username: '',
+      email: '',
+      password: '',
+    };
+  }
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       username: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
+    });
+  }
+
+  signup() {
+    this.signupRequestPayload.email = this.signupForm.get('email').value;
+    this.signupRequestPayload.username = this.signupForm.get('username').value;
+    this.signupRequestPayload.password = this.signupForm.get('password').value;
+
+    this.authService.signup(this.signupRequestPayload).subscribe((data) => {
+      console.log(data);
     });
   }
 }
